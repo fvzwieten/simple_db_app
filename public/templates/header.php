67aa0d2c34ab5b189f3736a8a1e61426c54b8d87
root <root@test3.lab1.example.com>
2019-02-20 11:05:40 +0100

    initial commit

---
+++ public/templates/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <h1><img src="images/redhat-logo.png" alt="Red Hat Rocks!" height="100" width="100"> 
+        Hands-on Lab
+	<img src="images/ansible-logo.png" alt="Ansible Rocks!" height="100" width="100"></h1> 
+	<title>Simple Database App</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>Simple Database App</h1>
